8c63909335cc5c8ec12e372e507a979e8f052386
aearavindh <aearavindh@gmail.com>
2018-09-03 14:08:04 +0530

    Odd or Even

--- OddEven.java
+++ OddEven.java
@@ -5,12 +5,13 @@ public class OddEven
 public static void main(String args[])
 {
 int num;
-System.out.println("Enter the number:");
 Scanner in = new Scanner(System.in);
 num = in.nextInt();
-if(num%2==0)
-System.out.println(num+" is even number");
+if(num<0)
+System.out.println("Invalid");
+elseif(num%2==0)
+System.out.println("Even");
 else
-System.out.println(num+" is odd number");
+System.out.println("Odd");
 }
 }
