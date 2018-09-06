b88bcc82303a37976ed0acb10dae41e029c7d987
aearavindh <aearavindh@gmail.com>
2018-09-06 23:43:20 +0530

    Hello N

---
+++ HelloN.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloN
+{
+public static void main(String args[])
+{
+int i,n;
+Scanner in = new Scanner(System.in);
+n = in.nextInt();
+for(i=0;i<n;i++)
+System.out.println("Hello");
+}
+} 
