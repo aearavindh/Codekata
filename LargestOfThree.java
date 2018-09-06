30e1c494a14b5216698632f3386d2b0ce8b1c63e
aearavindh <aearavindh@gmail.com>
2018-09-06 21:35:35 +0530

    Largest of Three

---
+++ LargestOfThree.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LargestOfThree
+{
+public static void main(String args[])
+{
+int a,b,c;
+Scanner in = new Scanner(System.in);
+a = in.nextInt();
+b = in.nextInt();
+c = in.nextInt();
+if(a>b && a>c)
+System.out.println(a);
+else if(b>c)
+System.out.println(b);
+else
+System.out.println(c);
+}
+}
