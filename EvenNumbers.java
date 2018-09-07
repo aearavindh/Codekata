5ce5516f9f69e216550298db3575c10ddac3a4a0
aearavindh <aearavindh@gmail.com>
2018-09-07 18:41:53 +0530

    Even Numbers

---
+++ EvenNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenNumbers
+{
+public static void main(String args[])
+{
+int a,b;
+Scanner in = new Scanner(System.in);
+a = in.nextInt();
+b = in.nextInt();
+for(a=a+1;a<b;a++)
+{
+if(a%2==0)
+System.out.print(a+" ");
+}
+}
+}
