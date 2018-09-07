04a1eec521a2e5a9d871421bb5b95af96eadf965
aearavindh <aearavindh@gmail.com>
2018-09-07 18:32:07 +0530

    Print Odd Numbers

---
+++ OddNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OddNumbers
+{
+public static void main(String args[])
+{
+int a,b,i;
+Scanner in = new Scanner(System.in);
+a = in.nextInt();
+b = in.nextInt();
+for(a=a+1;a<b;a++)
+{
+if(a%2!=0)
+System.out.print(a+" ");
+}
+}
+}
